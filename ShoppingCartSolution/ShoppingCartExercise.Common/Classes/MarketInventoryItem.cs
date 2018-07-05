98735e5d345121c04a8a888a16eac26d0a29a214
sean-mllrdev <sean.lagudas@mllrdev.com>
2018-07-05 19:09:12 +0800

    New Commit

---
+++ ShoppingCartSolution/ShoppingCartExercise.Common/Classes/MarketInventoryItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartExercise.Common.Classes
+{
+    public class MarketInventoryItem : InventoryItem
+    {
+        public MarketInventoryItem() : base()
+        {
+
+        }
+
+        public decimal Price { get; set; }
+        
+    }
+}
