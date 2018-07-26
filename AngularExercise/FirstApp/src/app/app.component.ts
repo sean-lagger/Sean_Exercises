911bb716eee7d2ac3cff5ef3de0941754e58cb7d
sean-mllrdev <sean.lagudas@mllrdev.com>
2018-07-26 10:26:24 +0800

    Commit before using Socket.io

--- AngularExercise/FirstApp/src/app/app.component.ts
+++ AngularExercise/FirstApp/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title: string = "Sean's first Angular Project";
+  private evtSource;
+
+  constructor(private http : HttpClient){
+    console.log("Root component created!");
+
+  }
+
+  ngOnInit(){
+    
+  }
+
+  onClickLogo(){
+    let element1 = document.getElementById("welcome");
+      element1.className = "greetings_animate";
+      element1.addEventListener("transitionend", function(){
+        document.getElementById("app_container").removeChild(element1);
+      })
+
+  }
 }
--- AngularExercise/FirstApp/src/app/app.module.ts
+++ AngularExercise/FirstApp/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { ChatScreenComponent } from './chat-screen/chat-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent,
+    ChatScreenComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
